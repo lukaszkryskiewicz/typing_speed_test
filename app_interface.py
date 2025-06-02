40cf5e3c6494106560200d5a06c1b246e525e819
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2025-06-03 01:09:32 +0200

    add timer

--- app_interface.py
+++ app_interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 import utilities
+from timer import Timer
 
 
 class AppInterface:
@@ -10,11 +11,12 @@ class AppInterface:
         self.window.config(padx=20, pady=20)
 
         self.score = 0
-        self.time_left = '00:00'
+        self.time_left = '00'
         self.words_list = []
         self.user_text = tk.StringVar()
 
         self._create_widgets()
+        self.timer = Timer(self.window)
         self.window.mainloop()
 
     def _create_widgets(self):
@@ -22,7 +24,8 @@ class AppInterface:
 
         tk.Label(text=f'Score: {self.score}', font=('Arial', 26)).grid(column=0, row=1, pady=20)
 
-        tk.Label(text=f'Time left: {self.time_left}', font=('Arial', 26)).grid(column=2, row=1, pady=20)
+        self.time_label = tk.Label(text=f'Time left: {self.time_left}', font=('Arial', 26))
+        self.time_label.grid(column=2, row=1, pady=20)
 
         tk.Button(text='Start', font=('Arial', 26), command=self.start_game).grid(column=1, row=2, pady=20)
 
@@ -33,9 +36,16 @@ class AppInterface:
         self.input.grid(column=0, row=6, columnspan=3, sticky="ew")
 
     def start_game(self):
-        self._create_words_list()
+        if not self.timer.timer:
+            self._create_words_list()
+            self.start_timer()
 
     def _create_words_list(self):
         self.words_list = utilities.generate_words()
         self.pattern.config(text=' '.join(self.words_list))
+
+    def start_timer(self):
+        self.timer.run_timer(10, self.time_label)
+        self.time_left = self.timer.time_left
+
 
---
+++ timer.py
@@ -0,0 +1,16 @@
+class Timer():
+    def __init__(self, window):
+        self.window = window
+        self.time_left = 0
+        self.timer = None
+
+
+    def run_timer(self, time, time_label):
+        self.time_left = time
+        time_label.config(text=f'Time left: {self.time_left if self.time_left >= 10 else f'0{self.time_left}'}')
+
+        if time > 0:
+            self.timer = self.window.after(1000, self.run_timer, time - 1, time_label)
+        else:
+            self.window.after_cancel(self.timer)
+            self.timer = None
