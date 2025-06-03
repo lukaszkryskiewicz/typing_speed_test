a492d35302abf438e9cd4afd0d86603a22d19a26
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2025-06-03 14:00:45 +0200

    add keyhandler and tags to pattern text

--- app_interface.py
+++ app_interface.py
@@ -13,10 +13,11 @@ class AppInterface:
         self.score = 0
         self.time_left = '00'
         self.words_list = []
-        self.user_text = tk.StringVar()
+        self.user_text = ''
 
         self._create_widgets()
         self.timer = Timer(self.window)
+        self.keyboard_listener = self.input.bind('<Key>', self.key_handler)
         self.window.mainloop()
 
     def _create_widgets(self):
@@ -29,11 +30,16 @@ class AppInterface:
 
         tk.Button(text='Start', font=('Arial', 26), command=self.start_game).grid(column=1, row=2, pady=20)
 
-        self.pattern = tk.Message(text='', bg='grey', fg='white', aspect=500, justify='center', padx=20, pady=20)
+        self.pattern = tk.Text(wrap='word', bg='grey', fg='white', padx=20, pady=20, height=4)
         self.pattern.grid(column=0, row=3, columnspan=3, rowspan=3, sticky="ew")
+        self.pattern.tag_config('correct', foreground='green')
+        self.pattern.tag_config('wrong', foreground='red')
+        self.pattern.tag_config('raw', foreground='white')
 
         self.input = tk.Text(height=4, padx=20, pady=20, spacing1=1)
         self.input.grid(column=0, row=6, columnspan=3, sticky="ew")
+
+
 
     def start_game(self):
         if not self.timer.timer:
@@ -42,10 +48,21 @@ class AppInterface:
 
     def _create_words_list(self):
         self.words_list = utilities.generate_words()
-        self.pattern.config(text=' '.join(self.words_list))
+        self.pattern.insert(1.0, self.words_list)
+        self.pattern.config(state='disabled')
 
     def start_timer(self):
         self.timer.run_timer(10, self.time_label)
         self.time_left = self.timer.time_left
+
+    def key_handler(self, event):
+        if not event.char.isalpha() and event.char == " " and event.keysym == 'BackSpace':
+            return 'break'
+        #fires _process_text after character is entered in input
+        self.input.after_idle(self._process_text, event.keysym)
+
+    def _process_text(self, key):
+        self.user_text = self.input.get("1.0", "end-1c")  # bez końcowego \n
+        utilities.compare_input(self.pattern, self.user_text, key, self.words_list)
 
 
--- utilities.py
+++ utilities.py
@@ -2,7 +2,7 @@ import requests
 
 
 def generate_words():
-    url =' https://random-word-api.herokuapp.com/word'
+    url ='https://random-word-api.herokuapp.com/word'
     params = {
         'length': 7,
         'number': 25,
@@ -16,3 +16,36 @@ def generate_words():
 
     return response.json()
 
+def compare_input(pattern, user_input, key, words_list):
+    user_input_list = user_input.split(' ')
+    current_word_index = len(user_input_list) - 1
+    current_char_index = len(user_input_list[current_word_index]) - 1
+    i = len(user_input_list[current_word_index])
+
+    current_pattern_word = words_list[current_word_index]
+    current_user_input_word = user_input_list[current_word_index]
+
+    # counts length of all previous words in a words_list and adds current_word_index value for spaces
+    base_offset = sum(len(words) for words in words_list[:current_word_index]) + current_word_index
+
+    if key == 'BackSpace':
+        start_del = f"1.0+{base_offset + current_char_index +1}c"
+        end_del = f"1.0+{base_offset + current_char_index + 2}c"
+        pattern.tag_remove("correct", start_del, end_del)
+        pattern.tag_remove("wrong", start_del, end_del)
+        pattern.tag_add('raw', start_del, end_del)
+
+    if current_char_index < 0 or i > len(current_pattern_word):
+        return
+
+    for idx in range(i):
+        correct = user_input_list[current_word_index][idx] == current_pattern_word[idx]
+        tag = 'correct' if correct else 'wrong'
+        char_offset = base_offset + idx
+
+        #forced by tkinter to use such fstring
+        start_i = f"1.0+{char_offset}c"
+        end_i = f"1.0+{char_offset + 1}c"
+
+        pattern.tag_remove("raw", start_i, end_i)
+        pattern.tag_add(tag, start_i, end_i)
